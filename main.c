c0ad27335f2541de8ba571ad7730403030b2dcb5
Richard-Lynch <rich753@hotmail.com>
2017-02-21 21:08:12 +0000

    linked list working

--- main.c
+++ main.c
@@ -9,40 +9,102 @@
 #include <time.h>
 #include <stdbool.h>
 
-pthread_mutex_t lock;
+typedef struct node node;
+
+typedef struct node{
+    int value;
+    node* next;
+} node;
+
+typedef struct llist{
+    node* head;
+    node* tail;
+} llist;
+
+void addNode(llist* List, int value){
+    if(List->head != NULL){ //if the list isnt empty
+        List->tail->next = (node*)(malloc(sizeof(node)));   //allocate for the node at add to the end
+        List->tail = List->tail->next;  //move the tail to the end
+        List->tail->value = value;  //add the value to the new node 
+        List->tail->next = NULL;    //make the new node point to null
+    } else {    //if the list is empty              
+        List->tail = (node*)malloc(sizeof(node));   //allocare a node at the end
+        List->head = List->tail;    //the start and the end are the same
+        List->tail->value = value;  //add the value to the new node
+        List->tail->next = NULL;    //make the new node point to null
+    }
+}
+
+void delNode(llist* List, int value){
+    node* prev = NULL;
+    node* curr = List->head;
+    while(curr != NULL){
+        if(curr->value == value){
+            if(curr == List->head){
+                List->head = curr->next;
+            } else if ( curr == List->tail){
+                prev->next = curr->next;
+                List->tail = prev;
+            } else {
+                prev->next = curr->next;
+            }
+            free(curr);
+            return;
+        } else {
+            prev = curr;
+            curr = curr->next;
+        }  
+    }
+    printf("\n");
+}
+
+void printList(llist* List){
+    node* curr = List->head;
+    while(curr != NULL){
+        printf("%d ", curr->value);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+void revList(llist* List){
+    List->tail = List->head;
+    node* prev = NULL;
+    node* curr = List->head;
+    node* next = NULL;
+    
+    while(curr != NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    List->head = prev;
+}
+
+
 
 int main(int argc, char *argv[])
 {
-// init mutex lock
-    if (pthread_mutex_init(&lock, NULL) != 0)
-    {
-        printf("\nlock mutex init failed\n");
-        return 1;
-    }
+    llist List;
+    List.head = NULL;
+    List.tail = NULL;
 
-// read args
-    int firstarg;
-    if (argc < 2 || (firstarg = atoi(argv[1])) <= 0)
-    { //convert the first arg to an int
-        //if there is not arg entered
-        printf("No Args Entered");
-        exit(1);
-    }
-
-// start clock
-    printf("Starting Clock\n");
-    clock_t start = clock();
-
-
-// stop the cock
-    printf("Stoping Clock\n");
-    clock_t tdiff = clock() - start;
-
-// destroy mutex
-    pthread_mutex_destroy(&lock);
+    addNode(&List, 2);
+    addNode(&List, 3);
+    addNode(&List, 4);
+    printList(&List);
+    revList(&List);
+    printList(&List);
+    delNode(&List, 2);
+    printList(&List);
 
 // done
     printf("Goodbye\n\n");
+
+    while(1){
+
+    }
 
     return 0;
 }
